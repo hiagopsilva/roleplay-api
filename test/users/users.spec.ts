7d708ebbad2353a3b3982b8e48f3195fece5c7aa
Hiago Prates <hiagohps360@gmail.com>
2022-09-01 13:25:22 -0300

    chore: update

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -4,7 +4,7 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('User', () => {
-  test.only('it should create an user', async (assert) => {
+  test.only('it should create an user', async () => {
     const userPayload = { email: 'hiago@gmail.com', username: 'hiago', password: '123' }
 
     await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
