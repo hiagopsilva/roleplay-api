b1baee2d2e9092cddca1ae5b1d46ac551aebcd8a
Hiago Prates <hiagohps360@gmail.com>
2022-09-01 13:48:46 -0300

    feat: migration user

--- database/migrations/1662050444160_users.ts
+++ database/migrations/1662050444160_users.ts
@@ -6,10 +6,10 @@ export default class extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-
-      /**
-       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
-       */
+      table.string('email').notNullable().unique()
+      table.string('username').notNullable().unique()
+      table.string('password').notNullable()
+      table.string('avatar').defaultTo('')
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
     })
