3827d1bd8bba74f3c2ee495243969a241a0e57dc
Hiago Prates <hiagohps360@gmail.com>
2022-09-01 13:24:47 -0300

    feat: test users

---
+++ test/users/users.spec.ts
@@ -0,0 +1,12 @@
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('User', () => {
+  test.only('it should create an user', async (assert) => {
+    const userPayload = { email: 'hiago@gmail.com', username: 'hiago', password: '123' }
+
+    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+  })
+})
