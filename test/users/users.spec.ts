0ace9c6029f6f0e827724bb4b0c75c4bc1550553
Hiago Prates <hiagohps360@gmail.com>
2022-09-01 13:42:40 -0300

    chore: update

--- test/users/users.spec.ts
+++ test/users/users.spec.ts
@@ -4,9 +4,21 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('User', () => {
-  test.only('it should create an user', async () => {
-    const userPayload = { email: 'hiago@gmail.com', username: 'hiago', password: '123' }
+  test.only('it should create an user', async (assert) => {
+    const userPayload = {
+      email: 'hiago@gmail.com',
+      username: 'hiago',
+      password: '123',
+      avatar: 'http://images.com/image/1',
+    }
 
-    await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+    const { body } = await supertest(BASE_URL).post('/users').send(userPayload).expect(201)
+
+    assert.exists(body.user, 'User undefined')
+    assert.exists(body.user.id, 'ID undefined')
+    assert.equal(body.user.email, userPayload.email)
+    assert.equal(body.user.username, userPayload.email)
+    assert.equal(body.user.password, userPayload.email)
+    assert.equal(body.user.avatar, userPayload.avatar)
   })
 })
