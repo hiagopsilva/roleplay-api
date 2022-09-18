e01126062edcb02d1bfbb483aea3bd706c390638
Hiago Prates <hiagohps360@gmail.com>
2022-09-18 16:05:44 -0300

    feat: using same token

--- app/Controllers/Http/PasswordsController.ts
+++ app/Controllers/Http/PasswordsController.ts
@@ -41,11 +41,13 @@ export default class PasswordsController {
       .whereHas('tokens', (query) => {
         query.where('token', token)
       })
+      .preload('tokens')
       .firstOrFail()
 
     userByToken.password = password
 
     await userByToken.save()
+    await userByToken.tokens[0].delete()
 
     return response.noContent()
   }
