90270040c28e1dca9a867bbf26833e5beaafc4e7
Hiago Prates <hiagohps360@gmail.com>
2022-09-24 13:58:12 -0300

    feat: test validate group

--- test/groups/group.spec.ts
+++ test/groups/group.spec.ts
@@ -6,7 +6,7 @@ import supertest from 'supertest'
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Group', async (group) => {
-  test.only('it should create a group', async (assert) => {
+  test('it should create a group', async (assert) => {
     const user = await UserFactory.create()
 
     const groupPayload = {
@@ -26,6 +26,13 @@ test.group('Group', async (group) => {
     assert.equal(body.group.location, groupPayload.location)
     assert.equal(body.group.chronic, groupPayload.chronic)
     assert.equal(body.group.master, groupPayload.master)
+  })
+
+  test.only('it should return 433 when required data is not provided', async (assert) => {
+    const { body } = await supertest(BASE_URL).post('/groups').send({}).expect(422)
+
+    assert.equal(body.code, 'BAD_REQUEST')
+    assert.equal(body.status, 422)
   })
 
   group.beforeEach(async () => {
