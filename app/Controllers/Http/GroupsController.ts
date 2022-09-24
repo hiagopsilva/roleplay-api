03aa188cc00d14cc0ac05f6c7cb4c0f68e62bf64
Hiago Prates <hiagohps360@gmail.com>
2022-09-24 13:21:21 -0300

    feat: group controller

---
+++ app/Controllers/Http/GroupsController.ts
@@ -0,0 +1,7 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class GroupsController {
+  public async store({ request, response }: HttpContextContract) {
+    return response.created({})
+  }
+}
