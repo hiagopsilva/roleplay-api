db91a64108347a95f060ca6dcdc778121a52977c
Hiago Prates <hiagohps360@gmail.com>
2022-09-24 13:21:49 -0300

    chore: update test

--- test/groups/group.spec.ts
+++ test/groups/group.spec.ts
@@ -1,11 +1,31 @@
 import Database from '@ioc:Adonis/Lucid/Database'
+import { UserFactory } from 'Database/factories'
 import test from 'japa'
 import supertest from 'supertest'
 
 const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Group', async (group) => {
-  test('', async (assert) => {})
+  test.only('it should create a group', async (assert) => {
+    const user = await UserFactory.create()
+
+    const groupPayload = {
+      name: 'Group 1',
+      description: 'Group 1 description',
+      schedule: 'Group 1 schedule',
+      location: 'Group 1 location',
+      chronic: 'Group 1 chronic',
+      master: user.id,
+    }
+    const { body } = await supertest(BASE_URL).post('/groups').send(groupPayload).expect(200)
+
+    assert.exists(body.group, 'Group undefined')
+    assert.equal(body.group.name, groupPayload.name)
+    assert.equal(body.group.description, groupPayload.description)
+    assert.equal(body.group.location, groupPayload.location)
+    assert.equal(body.group.chronic, groupPayload.chronic)
+    assert.equal(body.group.master, groupPayload.master)
+  })
 
   group.beforeEach(async () => {
     await Database.beginGlobalTransaction()
