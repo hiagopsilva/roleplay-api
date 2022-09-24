1c536e498a10583e3096bd4cf62e5297e3ff17ee
Hiago Prates <hiagohps360@gmail.com>
2022-09-24 13:11:39 -0300

    feat: test groups

---
+++ test/groups/group.spec.ts
@@ -0,0 +1,16 @@
+import Database from '@ioc:Adonis/Lucid/Database'
+import test from 'japa'
+import supertest from 'supertest'
+
+const BASE_URL = `http://${process.env.HOST}:${process.env.PORT}`
+
+test.group('Group', async (group) => {
+  test('', async (assert) => {})
+
+  group.beforeEach(async () => {
+    await Database.beginGlobalTransaction()
+  })
+  group.afterEach(async () => {
+    await Database.rollbackGlobalTransaction()
+  })
+})
